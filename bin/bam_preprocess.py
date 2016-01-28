36f567045173cbf21ced7b13af64fce218ab963f
hanfang <hanfang.cshl@gmail.com>
2016-01-27 21:39:34 -0500

    minor update

--- bin/bam_preprocess.py
+++ bin/bam_preprocess.py
@@ -14,64 +14,77 @@
 import os
 import sys
 import argparse
+import pybedtools as pbt
 import pysam
 import pandas as pd
 from collections import defaultdict
 
-class get_uniq_aln:
+## ----------------------------------------
+## class
+## ----------------------------------------
+class Get_Uniq_Aln:
     ''' define a class for extracting alignment based on MAPQ (>10)
     '''
-    def __init__(self, in_bam, user_mapq, out_bam):
-        self.in_bam    = in_bam
-        self.out_bam   = out_bam
+    def __init__(self, in_bam_fn, user_mapq, out_bam_fn):
+        self.in_bam_fn = in_bam_fn
+        self.out_bam_fn = out_bam_fn
         self.user_mapq = int(user_mapq)
 
     def filter_by_mapq(self):
-        self.samfile = pysam.AlignmentFile(self.in_bam, "rb")
-        self.uniq_mappers = pysam.AlignmentFile(self.out_bam, "wb", template=self.samfile)
+        self.pysam_hdl = pysam.AlignmentFile(self.in_bam_fn, "rb")
+        self.pysam_ftd = pysam.AlignmentFile(self.out_bam_fn, "wb", template=self.pysam_hdl)
 
         if self.user_mapq > 10:
             mapq = self.user_mapq
         else:
             mapq = 10
 
-        for read in self.samfile.fetch():
+        for read in self.pysam_hdl.fetch():
             if read.mapping_quality > mapq:
-                self.uniq_mappers.write(read)
+                self.pysam_ftd.write(read)
         
-        return (self.samfile)
+        return (self.pysam_ftd)
         print ("[execute]\t" + "filter by mapq of " + str(mapq) )
-        unique_mappers.close()
-        samfile.close()
+        pysam_ftd.close()
+        pysam_hdl.close()
         
-class get_aln_info:
+class Get_Aln_Info:
     ''' prepare a summary pandas table for alignment
     '''
-    def __init__(self, in_bam, bed): # , aln_info):
-        self.in_bam  = in_bam
-        self.bed     = bed
+    def __init__(self, pysam_hdl, bed): # , aln_info):
+        self.pysam_hdl = pysam_hdl
+        self.bed = bed
         #self.aln_info = aln_info
 
+    def make_bedtool(self):
+         self.bt_hdl = pbt.BedTool(self.pysam_hdl)
+         print(self.pysam_hdl)
+         print(self.bt_hdl)
+         
     def get_chr(self):
         hdl = list(dict())
-        for read in self.in_bam.fetch():    
-            hdl.append({'chr': self.in_bam.getrname(read.reference_id) , 'start': read.reference_start, 'end': read.reference_end, 'len':read.query_length, 'seq':read.query_sequence})
+        for read in self.in_bam_fn.fetch():    
+            hdl.append({'chr': self.in_bam_fn.getrname(read.reference_id) , 
+                        'start': read.reference_start, 
+                        'end': read.reference_end, 
+                        'len': read.query_length, 
+                        'cigar': read.cigar, 
+                        'seq': read.query_sequence})
 
-        hdl_df = pd.DataFrame(hdl,columns=['chr', 'start', 'end', 'len','seq'])
-        print (hdl_df)
-        return (hdl_df)
-        # bed = pysam.asBed(self.bed)
-        #i=0
-        #while i < fasta.nreferences:
-        #    print(self.in_bam.getrname(i))
-        #    i+=1   
+        hdl_df = pd.DataFrame(hdl, columns = ['chr', 'start', 'end', 'len','cigar','seq'])
 
+        print(len(hdl_df.cigar))
+        return(hdl_df)
+ 
+## ----------------------------------------
+## the main work
+## ----------------------------------------
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", help="input bam file")
-    parser.add_argument("-b", help="a BED file with start codons")
-    parser.add_argument("-q", help="minimum mapq allowed")
-    parser.add_argument("-o", help="output bam file")
+    parser.add_argument("-i", help = "input bam file")
+    parser.add_argument("-b", help = "a BED file with start codons")
+    parser.add_argument("-q", help = "minimum mapq allowed")
+    parser.add_argument("-o", help = "output bam file")
 
     ## check if there is any argument
     if len(sys.argv) <= 1: 
@@ -81,13 +94,18 @@ if __name__ == '__main__':
         args = parser.parse_args()
 
     ## process the file if the input files exist
-    if (args.i!=None) & (args.b!=None) & (args.q!=None) & (args.o!=None) :
+    if (args.i!=None and 
+        args.b!=None and 
+        args.q!=None and 
+        args.o!=None) :
+        
         print ("[status]\tprocessing the input bam file: " + args.i)
-        unique_mappers = get_uniq_aln( args.i, args.q, args.o)
-        unique_mappers.filter_by_mapq()
+        fetch_aln = Get_Uniq_Aln(args.i, args.q, args.o)
+        fetch_aln.filter_by_mapq()
 
-        get_aln = get_aln_info(unique_mappers.samfile,args.b)
-        get_aln.get_chr()
+        gen_aln = Get_Aln_Info(fetch_aln.pysam_hdl, args.b)
+        gen_aln.make_bedtool()
+        
     else:
         print ("[error]\tmissing argument")
         parser.print_usage() 
