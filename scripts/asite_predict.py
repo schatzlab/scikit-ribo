bf8ba16c4e348f45fcb51d0642315d555b781fe3
hanfang <hanfang.cshl@gmail.com>
2016-03-16 21:48:16 -0400

    [update] add a function for predicting a-site from testing data

--- scripts/asite_predict.py
+++ scripts/asite_predict.py
@@ -49,33 +49,46 @@ class VisualizeAsite:
         g0.map(plt.hist, "a_site", color="steelblue", bins=bins, lw=0,normed=True)
         g0.set(xticks=[12,15,18,21])
         plt.gcf()
-        plt.savefig( asite_fn +".asite.pdf" )
+        plt.savefig( self.asite_fn +".asite.pdf" )
         plt.clf()
 
 class TrainModel:
     ''' define a class for model training - a-site prediction
     '''
-    def __init__(self, object):
-        self.asite_df = object.asite_df
-        self.df_colnames = list(object.asite_df.columns.values)
+    def __init__(self, object, testing_fn):
+        self.traning_df = object.asite_df
+        self.asite_fn = object.asite_fn
+        self.df_colnames = list(self.traning_df.columns.values)
         self.df_colnames.remove("a_site")
-        self.X = np.array( pd.get_dummies( object.asite_df[self.df_colnames] ))
-        self.y = np.array(object.asite_df["a_site"])
+        self.df_colnames.remove("gene_name")
+        self.X = np.array( pd.get_dummies(self.traning_df[self.df_colnames]) ) # .astype(np.int8)
+        self.y = np.array( self.traning_df["a_site"])
+        self.testing_fn = testing_fn
+        self.testing_df = pd.read_table(self.testing_fn,  header=0)
 
     def rf_fit(self):
-
         ## feature selection
         self.clf = RandomForestClassifier(max_features=None, n_jobs=-1)
         self.clf = self.clf.fit(self.X, self.y)
         self.importances = self.clf.feature_importances_
-        transformer = SelectFromModel(self.clf, prefit=True, threshold=0.05)
-        self.selected_X = transformer.transform(self.X)
+        self.transformer = SelectFromModel(self.clf, prefit=True, threshold=0.05)
+        self.selected_X = self.transformer.transform(self.X)
+
+        ## find the number of select features
+        n_selected_features = self.selected_X.shape[1]
+        print (self.X.shape, self.selected_X.shape,  flush=True)
+
+        ## define a new classifier for reduced features
+        self.new_clf = RandomForestClassifier(max_features=None, n_jobs=-1)
+        self.new_clf = self.new_clf.fit(self.selected_X, self.y)
+        print("[status]\tNumber of selected features:\t"+ str(n_selected_features), flush=True)
 
         ## cross validation
-        scores = cross_validation.cross_val_score(self.clf, self.selected_X, self.y, cv=10)
-        print("[status]\tAccuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2))
+        scores = cross_validation.cross_val_score(self.new_clf, self.selected_X, self.y, cv=10)
+        print("[status]\tAccuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2), flush=True)
 
     def rf_importance(self):
+        ## compute the std and index for the feature importance
         std = np.std([tree.feature_importances_ for tree in self.clf.estimators_],    axis=0)
         indices = np.argsort(self.importances)[::-1]
 
@@ -88,26 +101,40 @@ class TrainModel:
         plt.xlim([-1, 10])
         plt.ylim([0, 1])
         plt.gcf()
-        plt.savefig(asite_fn + ".feature_importances.pdf", facecolor="white")
+        plt.savefig( self.asite_fn + ".feature_importances.pdf", facecolor="white")
+
+    def rf_predict(self):
+        ## create df for testing data
+        testing_df = pd.read_table(self.testing_fn,  header=0)
+        testing_df_colnames = list(testing_df.columns.values)
+        names_to_exclude = set(["chrom", "start", "end", "name", "score", "strand", "read"])
+        testing_df_colnames_subset = [x for x in testing_df_colnames if x not in names_to_exclude]
+        testing_df_colnames_subset.remove("gene_name")
+        testing_X = np.array( pd.get_dummies(testing_df[testing_df_colnames_subset]) ) # .astype(np.int8)
+
+        ## selected a subset of features and predict a-site
+        selected_testing_X = self.transformer.transform(testing_X)
+        testing_df["a_site"] = self.new_clf.predict(selected_testing_X)
+        testing_df_out = testing_df[["chrom", "start", "end", "name", "score", "strand", "read", "a_site", \
+                                     "read_length", "offset", "start_seq", "end_seq", "gene_name"]]
+        testing_df_out.to_csv(path_or_buf=self.testing_fn + '.predicted.txt', sep='\t', header=True, index=False)
 
     def svm_fit(self):
-
         ## grid search
         self.clf = svm.SVC()
         param_grid = [ { 'C': [ 0.01, 0.1, 1, 10, 100, 1000, 10000] } ]
         self.clf_gs = GridSearchCV(estimator=self.clf, param_grid=param_grid, n_jobs=-1)
         self.clf_gs.fit(self.X, self.y)
-        print("[status]\t best estimator parameters: c=", self.clf_gs.best_estimator_.C)
+        print("[status]\t best estimator parameters: c=", self.clf_gs.best_estimator_.C, flush=True)
 
         ## model fitting and cross validation
         self.clf = svm.SVC( C=self.clf_gs.best_estimator_.C)
         scores = cross_validation.cross_val_score(self.clf, self.selected_X, self.y, cv=10)
-        print("[status]\tAccuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2))
+        print("[status]\tAccuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2), flush=True)
 
     def roc_curve(self):
         ''' define a class for plotting multi-class roc curve
         '''
-
         # shuffle and split training and test sets
         self.OVR_clf = OneVsRestClassifier(self.clf)
         self.y = label_binarize(self.y, classes=[12,13,14,15,16,17,18])
@@ -146,7 +173,7 @@ class TrainModel:
         plt.tick_params(axis='both', which='major', labelsize=18)
         plt.legend(loc="lower right",fontsize=12)
         plt.gcf()
-        plt.savefig(asite_fn + ".roc.pdf")
+        plt.savefig( self.asite_fn + ".roc.pdf")
 
 ## ----------------------------------------
 ## the main work
@@ -154,6 +181,7 @@ class TrainModel:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", help="input a-site file, required")
+    parser.add_argument("-t", help="input testing data for the entire CDS region, required")
     parser.add_argument("-c", help="classifier to use, random forest (rf) or "
                                    "\support vector machine (svm), optional, default: rf", default="rf")
 
@@ -165,24 +193,31 @@ if __name__ == '__main__':
         args = parser.parse_args()
 
     ## process the file if the input files exist
-    if (args.i != None):
-        print("[status]\tprocessing the input file: " + args.i)
+    if (args.i != None and args.t != None):
+        print("[status]\tprocessing the input file: " + args.i, flush=True)
         asite_fn = args.i
+        cds_fn = args.t
         classifier = args.c
 
-        print("[execute]\tplotting the a-site location distribution from " + str(asite_fn))
+        print("[execute]\tplotting the a-site location distribution from " + str(asite_fn), flush=True)
         asite_loc = VisualizeAsite(asite_fn)
         asite_loc.plot()
 
-        print("[execute]\tperform cross validation on the training data")
-        model = TrainModel(asite_loc)
+        print("[execute]\tstart the process of a_site prediction", flush=True)
+        model = TrainModel(asite_loc, cds_fn)
+
         if classifier == "rf":
+            print("[execute]\tperform model training and cross validation on the training data", flush=True)
             model.rf_fit()
+            print("[execute]\tplotting the bar plot of the feature importance", flush=True)
             model.rf_importance()
+            print("[execute]\tpredicting the a-site from the testing data", flush=True)
+            model.rf_predict()
+
         elif classifier == "svm":
             model.svm_fit()
             model.roc_curve()
 
     else:
-        print("[error]\tmissing argument")
+        print("[error]\tmissing argument", flush=True)
         parser.print_usage()
