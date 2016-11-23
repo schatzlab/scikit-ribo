30871c0abd065eaeb8ccbc0b9b43f8378e57ff9e
hanfang <hanfang.cshl@gmail.com>
2016-11-22 23:26:04 -0500

    [update] change offset calculation, now use df, much faster

--- scripts/bam_preprocess.py
+++ scripts/bam_preprocess.py
@@ -20,12 +20,9 @@ import pysam
 import pandas as pd
 import numpy as np
 import csv
-import multiprocessing
 import errno
-import pyximport; pyximport.install()
-import skrCython as skrc
-from timeit import default_timer as timer
-from collections import defaultdict
+from datetime import datetime
+# import dask.dataframe as dd
 
 
 class processAln(object):
@@ -44,11 +41,7 @@ class processAln(object):
         self.orf = pbt.BedTool(orf).filter(lambda x: x.chrom != 'chrM')
         self.orf = self.orf.sort()
         self.posRanges = posRanges
-        self.posDic = defaultdict(list)
-        ## temp
-        #with open(self.outBam + '.reads.txt', 'r') as f:
-        #    for line in f:
-        #        self.reads.append(line.rstrip("\n").split("\t"))
+        self.offsets = None
 
     def filterRegion(self):
         # find overlapping regions
@@ -69,36 +62,43 @@ class processAln(object):
         inBamHdl = pysam.AlignmentFile(self.inBam, "rb")
         outBamHdl = pysam.AlignmentFile(self.outBam, "wb", template=inBamHdl)
         ## read a bam file and extract info
+        cigar_to_exclude = set([1,2,3,4,5]) #set(['I','D','S','H'])
         for read in inBamHdl.fetch():
-            cigar_to_exclude = set([1,2,3,4,5]) #set(['I','D','S','H'])
-            cigars = [c[0] for c in read.cigartuples]
+            cigars = set([c[0] for c in read.cigartuples])
             # start_nt, end_nt = read.query_sequence[:2], read.query_sequence[-2:][::-1]
             # edges = set(list(start_nt + end_nt))
             # filter the bam file
             if read.mapping_quality > self.mapq and \
             self.minRL <= read.query_length <= self.maxRL and \
-            not any(c in cigar_to_exclude for c in cigars): # and 'N' not in edges:
+            len(cigars.intersection(cigar_to_exclude)) == 0 and \
+            read.reference_id != 'chrM': # and 'N' not in edges:
+                read.mapping_quality = read.query_length
                 outBamHdl.write(read)
-                self.reads.append([read.query_name, read.query_length])
-                # self.reads.append([read.query_name, read.query_length, start_nt, end_nt])
+                # self.reads.append([read.query_name, read.query_length]) # start_nt, end_nt])
         inBamHdl.close()
         outBamHdl.close()
+        print("[status]\tFinished filtering the bam file", flush=True)
         # save the bedtool to local
         self.bedtool = pbt.BedTool(self.outBam)
-        self.bedtool = self.bedtool.bam_to_bed(bed12=True).filter(lambda x: x.chrom != 'chrM')
+        self.bedtool = self.bedtool.bam_to_bed(bed12=True)
         self.bedtool.saveas(self.outBam + '.bed')
-        # export reads to local
-        with open(self.outBam + '.reads.txt', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile, delimiter='\t')
-            writer.writerows(self.reads)
 
     def posIndex(self):
         ## create a dict to store the position read-frame and index info
+        self.offsets = []
         with open(self.posRanges, 'r') as f:
             next(f)
             for line in f:
-                gene, ranges = line.rstrip("\n").split("\t")
-                self.posDic[gene] = [(int(i[0]), int(i[1]), int(i[2])) for i in [j.split(",") for j in ranges.split("|") ]]
+                gene, chr, strand, ranges = line.rstrip("\n").split("\t")
+                boxes = [(int(i[0]), int(i[1]), int(i[2])) for i in [j.split(",") for j in ranges.split("|") ]]
+                if strand == "-": boxes = boxes[::-1]
+                if strand == "+":
+                    self.offsets.extend([[gene, pos, box[2]+(pos-(box[0]-15))%3 ] for box in boxes for pos in range(box[0]-15, box[1]+12)])
+                else:
+                    self.offsets.extend([[gene, pos, box[2]+((box[1]+15)-pos)%3 ] for box in boxes for pos in range(box[1]+15, box[0]-12, -1)])
+        self.offsets = pd.DataFrame(self.offsets, columns=["gene_name", "pos", "offset"])
+        # self.offsets = dd.from_pandas(self.offsets, npartitions=2)
+        # self.posDic[gene] = [(int(i[0]), int(i[1]), int(i[2])) for i in [j.split(",") for j in ranges.split("|") ]]
 
     def sortBam(self):
         self.bedtool = pbt.BedTool(self.outBam)
@@ -108,93 +108,79 @@ class processAln(object):
         #os.system('sort {0} -k1,1 -nk4,4 > {1}'.format(self.bedtool.fn, tmpfile))
         #self.bedtool = pbt.BedTool(tmpfile)
 
-    def trainingData(self):
+    def makeTrainingData(self):
         # intersect with start codons
         self.bedtool = pbt.BedTool(self.outBam + '.bed')
         trainingData = self.bedtool.intersect(self.startCodons, wa=True, wb=True, sorted=True)
-
-        trainingDf = trainingData.to_dataframe(names=['chrom', 'start', 'end', 'name', 'score', 'strand', 'thickStart',
-                                                      'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts',
-                                                      'sc_chrom', 'sc_start', 'sc_end', 'gene_name', 'sc_score',
-                                                      'gene_strand'],
-                                               dtype={'start':'int','end':'int', 'score':'int','sc_start':'int',
-                                                      'sc_end':'int','sc_score':'int'} )
-        self.readsDf = pd.DataFrame(self.reads, columns=['name', 'read_length'])
-        #self.readsDf = pd.DataFrame(self.reads, columns=['name', 'read_length', 'start_nt', 'end_nt'])
-
-        ## retrieve the read length and seq information from the bam file
-        trainingDf = pd.merge(trainingDf, self.readsDf, on='name')
+        time = str(datetime.now())
+        print("[status]\tFinished intersecting the bedtool with start codons:", time, flush=True)
+        # convert bedtool to df
+        trainingDf = trainingData.to_dataframe(names=['chrom', 'start', 'end', 'name', 'read_length', 'strand',
+                                                      'thickStart', 'thickEnd', 'itemRgb', 'blockCount', 'blockSizes',
+                                                      'blockStarts', 'sc_chrom', 'sc_start', 'sc_end', 'gene_name',
+                                                      'sc_score', 'gene_strand'])
         ### a-site
         trainingDf['asite'] = np.where(trainingDf['gene_strand'] == '+',
                                        trainingDf['sc_start'] - trainingDf['start'] + 3,
                                        trainingDf['end'] - trainingDf['sc_end'] + 3 )
         ## phasing
-        trainingDf['five_offset'] = trainingDf.apply(lambda x: self.getDistance(x['gene_name'], x['start'], x['end'],
-                                                               x['gene_strand'], self.posDic, 5), axis=1)
+        trainingDf = pd.merge(trainingDf, self.offsets, left_on=["gene_name", "start"], right_on=["gene_name","pos"])
+        trainingDf.rename(columns={'offset':'five_offset'}, inplace=True)
+        trainingDf = pd.merge(trainingDf, self.offsets, left_on=["gene_name", "end"], right_on=["gene_name","pos"])
+        trainingDf.rename(columns={'offset':'three_offset'}, inplace=True)
 
-        trainingDf['three_offset'] = trainingDf.apply(lambda x: self.getDistance(x['gene_name'], x['start'], x['end'],
-                                                                x['gene_strand'], self.posDic, 3), axis=1)
         ## filter a read by whether it has a-site that satisfies [12,18]
         trainingDf = trainingDf[((trainingDf['asite'] >= 12) & (trainingDf['asite'] <= 18))]
-        ## slice the dataframe to the variables needed for training data
-        trainingDataOut = trainingDf[["asite", "read_length", "five_offset", "three_offset", "gene_strand"]]
-        # trainingDataOut = trainingDf[["asite", "read_length", "five_offset", "three_offset", "start_nt", "end_nt", "gene_strand"]]
-        trainingDataOut.to_csv(path_or_buf=self.outBam + '.asite.txt', sep='\t', header=True, index=False)
+        trainingDf = trainingDf[(trainingDf['asite'] >= trainingDf['read_length'] / 2 - 1)]
+        ## slice the dataframe to the variables needed for training data, removed "start_nt", "end_nt"
+        trainingDf = trainingDf[["asite", "read_length", "five_offset", "three_offset", "gene_strand"]]
+        trainingDf.to_csv(path_or_buf=self.outBam + '.training.txt', sep='\t', header=True, index=False)
+        '''
+        ## use dask to do merging
+        #Construct a dask objects from a pandas objects
+        trainingDf = dd.from_pandas(trainingDf, npartitions=2)
+        trainingDf = dd.merge(trainingDf, self.offsets, left_on=["gene_name", "start"], right_on=["gene_name","pos"])
+        trainingDf = trainingDf.rename(columns={'offset':'five_offset'})
+        trainingDf = dd.merge(trainingDf, self.offsets, left_on=["gene_name", "end"], right_on=["gene_name","pos"])
+        trainingDf = trainingDf.rename(columns={'offset':'three_offset'})
+        trainingDf = trainingDf[((trainingDf['asite'] >= 12) & (trainingDf['asite'] <= 18))]
+        trainingDf = trainingDf[(trainingDf['asite'] >= trainingDf['read_length'] / 2 - 1)]
+        trainingDf = trainingDf[["asite", "read_length", "five_offset", "three_offset", "gene_strand"]].compute()
+        trainingDf.to_csv(self.outBam + '.asite.txt', sep='\t', header=True, index=False)
+        '''
 
-    def testingData(self):
+    def makeTestingData(self):
         ## create pandas df from bedtools intersect
         self.bedtool = pbt.BedTool(self.outBam + '.bed')
         testingData = self.bedtool.intersect(self.orf, wa=True, wb=True, sorted=True)
-        testingDf = testingData.to_dataframe(names=['chrom', 'start', 'end', 'name', 'mapq', 'strand', 'thickStart',
+        time = str(datetime.now())
+        print("[status]\tFinished intersecting the bedtool with ORFs:", time, flush=True)
+        testingDf = testingData.to_dataframe(names=['chrom', 'start', 'end', 'name', 'read_length', 'strand', 'thickStart',
                                                     'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts',
                                                     'gene_chrom', 'gene_start', 'gene_end', 'gene_name', 'gene_score',
                                                     'gene_strand', 'gene_thickStart', 'gene_thickEnd', 'gene_itemRgb',
                                                     'gene_blockCount', 'gene_blockSizes', 'gene_blockStarts'],
-                                             dtype={'start':'int','end':'int', 'mapq':'int', 'gene_start':'int',
-                                                    'gene_end':'int', 'gene_score':'int',
-                                                    'blockSizes':'object','blockStarts':'object',
+                                             dtype={'blockSizes':'object','blockStarts':'object',
                                                     'gene_blockSizes':'object','gene_blockStarts':'object'})
-        ## compute offset
-        #start = timer()
-        testingDf['five_offset'] = testingDf.apply(lambda x: self.getDistance(x['gene_name'], x['start'], x['end'],
-                                                             x['gene_strand'], self.posDic, 5), axis=1)
-        testingDf['three_offset'] = testingDf.apply(lambda x: self.getDistance(x['gene_name'], x['start'], x['end'],
-                                                              x['gene_strand'], self.posDic, 3), axis=1)
-        #end = timer()
-        #print(end-start, flush=True)
-        testingDf = testingDf[(testingDf['five_offset'] != -1) & (testingDf['three_offset'] != -1)]
-        testingDf = pd.merge(testingDf, self.readsDf, on='name', how='inner')
+        # merge df
+        testingDf = pd.merge(testingDf, self.offsets, left_on=["gene_name", "start"], right_on=["gene_name","pos"])
+        testingDf.rename(columns={'offset':'five_offset'}, inplace=True)
+        testingDf = pd.merge(testingDf, self.offsets, left_on=["gene_name", "end"], right_on=["gene_name","pos"])
+        testingDf.rename(columns={'offset':'three_offset'}, inplace=True)
         ## slice the dataframe to the variables needed for training data
-        testingDataOut = testingDf[["read_length", "five_offset", "three_offset", "gene_strand", "chrom", "start", "end"]]
-        # testingDataOut = testingDf[["read_length", "five_offset", "three_offset", "start_nt", "end_nt", "gene_strand"]
-        #                            "gene_name", "chrom", "start", "end", "name", "strand"]]
-        testingDataOut.to_csv(path_or_buf=self.outBam + '.cds.txt', sep='\t', header=True, index=False)
-
-    def getDistance(self, gene_name, start, end, gene_strand, dic, mode):
-        pos_ranges = dic[gene_name]
-        if gene_strand == "-": pos_ranges = pos_ranges[::-1]
-        num_regions = len(pos_ranges)
-        if mode == 5:
-            pos = start + 15 if gene_strand == "+" else end - 15
-            return self.offsetHelper(num_regions, pos_ranges, gene_strand, pos)
-        elif mode == 3:
-            pos = end - 12 if gene_strand == "+" else start + 12
-            return self.offsetHelper(num_regions, pos_ranges, gene_strand, pos)
-        else:
-            exit(errno.EINVAL)
-
-    def offsetHelper(self, numRegions, posRanges, geneStrand, pos):
-            i = 0
-            while i < numRegions:
-                left, right, phase = posRanges[i]
-                if left <= pos <= right:
-                    up = left if geneStrand == "+" else right
-                    offset = ( abs(pos - up) + phase) % 3
-                    return offset
-                else:
-                    i += 1
-            return -1
-
+        testingDf = testingDf[["read_length", "five_offset", "three_offset", "gene_strand", "chrom", "start", "end"]]
+        testingDf.to_csv(path_or_buf=self.outBam + '.testing.txt', sep='\t', header=True, index=False)
+        '''
+        ## use dask to do merging
+        #Construct a dask objects from a pandas objects
+        testingDf = dd.from_pandas(testingDf, npartitions=2)
+        testingDf = dd.merge(testingDf, self.offsets, left_on=["gene_name", "start"], right_on=["gene_name","pos"])
+        testingDf = testingDf.rename(columns={'offset':'five_offset'})
+        testingDf = dd.merge(testingDf, self.offsets, left_on=["gene_name", "end"], right_on=["gene_name","pos"])
+        testingDf = testingDf.rename(columns={'offset':'three_offset'})
+        testingDf = testingDf[["read_length", "five_offset", "three_offset", "gene_strand", "chrom", "start", "end"]].compute()
+        testingDf.to_csv(self.outBam + '.cds.txt', sep='\t', header=True, index=False)
+        '''
 
 ## ----------------------------------------
 ## the main work
@@ -226,6 +212,8 @@ if __name__ == '__main__':
         start_codons = args.p + ".sort.start.bed"
         orf = args.p + ".sort.CDS.bed"
         pos_ranges = args.p + ".pos_ranges.txt"
+        time = str(datetime.now())
+        print("[status]\tStart the module at", time, flush=True)
         print("[status]\tprocessing the input bam file: " + in_bam_fn, flush=True)
         print("[status]\toutput bam file name: " + out_bam_fn, flush=True)
         print("[status]\treading the start codon BED file: " + start_codons, flush=True)
@@ -241,12 +229,15 @@ if __name__ == '__main__':
         print("[execute]\timport the position ranges", flush=True)
         aln.posIndex()
         print("[execute]\tfilter out un-reliable alignment from bam files", flush=True)
-        aln.filterBam()
-        print("[execute]\tcreate dataframe - training data", flush=True)
-        aln.trainingData()
-        print("[execute]\tcreate dataframe - testing data", flush=True)
-        aln.testingData()
-        print("[status]\tBam pre-processing finished", flush=True)
+        #aln.filterBam()
+        time = str(datetime.now())
+        print("[execute]\tcreate dataframe - training data", time, flush=True)
+        aln.makeTrainingData()
+        time = str(datetime.now())
+        print("[execute]\tcreate dataframe - testing data", time, flush=True)
+        aln.makeTestingData()
+        time = str(datetime.now())
+        print("[status]\tBam pre-processing finished at", time, flush=True)
 
     else:
         print("[error]\tmissing argument", flush=True)
