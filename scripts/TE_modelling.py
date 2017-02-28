dd7465a5fbaa355d1810e496c4439fe7984dfcbf
hanfang <hanfang.cshl@gmail.com>
2017-02-28 12:18:18 -0500

    [update] touch up pep8

--- scripts/TE_modelling.py
+++ scripts/TE_modelling.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 
-## ----------------------------------------
-## scikit-ribo
-## ----------------------------------------
-## a module for fitting the GLM of TE
-## ----------------------------------------
-## author: Han Fang
-## contact: hanfang.cshl@gmail.com
-## website: hanfang.github.io
-## date: 3/31/2016
-## ----------------------------------------
+# ----------------------------------------
+# scikit-ribo
+# ----------------------------------------
+# a module for fitting the GLM of TE
+# ----------------------------------------
+# author: Han Fang
+# contact: hanfang.cshl@gmail.com
+# website: hanfang.github.io
+# date: 3/31/2016
+# ----------------------------------------
 
 from __future__ import print_function
 import sys
 import argparse
-import numpy as np
 import statsmodels
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
@@ -26,11 +25,11 @@ from statsmodels.base.model import GenericLikelihoodModel
 import rpy2.robjects.packages as rpackages
 import rpy2.robjects as ro
 import rpy2.robjects.numpy2ri as n2r
-#from glmnet import PoissonNet
-#from memory_profiler import profile
+# from glmnet import PoissonNet
+# from memory_profiler import profile
 
 
-class glmTE(object):
+class GlmTE(object):
     """ perform glm for TE estimation
     """
     def __init__(self, fn=None, unMappableFn=None, tpmLB=1):
@@ -42,6 +41,8 @@ class glmTE(object):
         self.fn = fn
         self.unMappableFn = unMappableFn
         self.tpmLB = tpmLB
+        self.unMappable = None
+        self.df = None
 
     def loadDat(self):
         """
@@ -55,14 +56,15 @@ class glmTE(object):
         codons = self.df[["gene", "codon_idx"]]
         codonLen = codons.groupby('gene').max().reset_index()
         codonLen.columns = ['gene', "numCodons"]
-        codonLen["numCodons"] = codonLen["numCodons"] - 8
-        self.df = pd.merge(self.df, codonLen, how = "left", on = ["gene"])
+        codonLen["numCodons"] -= 8 # removed the appended 3 utr files
+        self.df = pd.merge(self.df, codonLen, how="left", on=["gene"])
         # rolling mean of 2' structure pairing probabilities
         paring = self.df[["gene", "codon_idx", "pair_prob"]]
         rollingAvg = paring.groupby("gene").rolling(window=11, center=True).mean().reset_index(0, drop=True)
         rollingAvg.columns = ["gene", "codon_idx", "avg_prob"]
         # merge 2' prob
-        self.df = pd.merge(self.df, rollingAvg, how='left', on=["gene", 'codon_idx']).drop(["pair_prob"], axis=1).dropna()
+        self.df = pd.merge(self.df, rollingAvg, how='left', on=["gene", 'codon_idx']).drop(["pair_prob"], axis=1)
+        self.df = self.df.dropna()
 
     def filterDf(self):
         """
@@ -79,8 +81,8 @@ class glmTE(object):
         codonLen = self.df[["gene", "numCodons"]].drop_duplicates(subset=["gene"])
         gene = pd.merge(gene, codonLen, on="gene")
         gene["rpkm"] = gene["riboCnt"] * 1000000000 / (np.sum(gene["riboCnt"]) * gene["numCodons"] * 3)
-        gene["tpm"] = gene["rpkm"]  * 1000000 / np.sum(gene["rpkm"])
-        gene = gene[(gene["tpm"]>=self.tpmLB)][["gene"]]
+        gene["tpm"] = gene["rpkm"] * 1000000 / np.sum(gene["rpkm"])
+        gene = gene[(gene["tpm"] >= self.tpmLB)][["gene"]]
         self.df = pd.merge(self.df, gene, on="gene")
         # 3. exclude genes w/ low RNA TPM, ribosome count > 100
         self.df = self.df[(self.df["TPM"] >= self.tpmLB)]
@@ -90,9 +92,9 @@ class glmTE(object):
         self.df = bt.to_dataframe(names=colNames)
         # 5. remove genes that have too few codons remained (<10)
         geneFilter = self.df.groupby('gene').size().reset_index(name="remained")
-        geneFilter["keep"] = np.where(geneFilter.remained>10, True, False)
-        geneFilter = geneFilter[(geneFilter["keep"] == True)][['gene']]
-        self.df = pd.merge(self.df, geneFilter, how = "inner", on = ["gene"])
+        geneFilter["keep"] = np.where(geneFilter.remained > 10, True, False)
+        geneFilter = geneFilter[(geneFilter["keep"] is True)][['gene']]
+        self.df = pd.merge(self.df, geneFilter, how="inner", on=["gene"])
         # 6. exclude stop codons: TAG, TAA, TGA
         self.df = self.df[((self.df["codon"] != 'TAG') & (self.df["codon"] != 'TAA') & (self.df["codon"] != 'TGA'))]
         numGenes = np.unique(self.df.gene).shape[0]
@@ -119,40 +121,45 @@ class glmTE(object):
         self.df.to_csv(path_or_buf='filtered.txt', sep='\t', header=True, index=False, float_format='%.4f')
 
     def nbGlm(self):
-        ## define model formula
+        # define model formula
         self.df = self.df[['ribosome_count', 'gene', 'codon', 'avgProb_scaled', 'logTPM_scaled']]
         formula = 'ribosome_count ~ C(gene) + C(codon) + avgProb_scaled'
         sys.stderr.write("[status]\tFormula: " + str(formula) + "\n")
-        ## define model fitting options
-        sovler = "IRLS" # "lbfgs"
-        tolerence = 1e-4
+        # define model fitting options
+        solver = "IRLS"  # "lbfgs"
+        tolerance = 1e-4
         numIter = 100
-        sys.stderr.write("[status]\tSolver: " + sovler + "\n")
-        sys.stderr.write("[status]\tConvergence tolerance: " + str(tolerence) + "\n")
+        sys.stderr.write("[status]\tSolver: " + solver + "\n")
+        sys.stderr.write("[status]\tConvergence tolerance: " + str(tolerance) + "\n")
         sys.stderr.write("[status]\tMaxiter: " + str(numIter) + "\n")
-        ## model fitting NegativeBinomial GLM
-        sys.stderr.write("[status]\tModel: smf.glm(formula, self.df, family=sm.families.NegativeBinomial(), offset=self.df['logTPM_scaled']" + "\n")
-        mod = smf.glm(formula, self.df, family=sm.families.NegativeBinomial(), offset=self.df['logTPM_scaled'])
-        res = mod.fit(method=sovler, tol=tolerence, maxiter=numIter)
-        ## print model output
+        # model fitting NegativeBinomial GLM
+        sys.stderr.write("[status]\tModel: smf.glm(formula, self.df, family=sm.families.NegativeBinomial(), "
+                         "offset=self.df['logTPM_scaled']" + "\n")
+        model = smf.glm(formula, self.df, family=sm.families.NegativeBinomial(), offset=self.df['logTPM_scaled'])
+        res = model.fit(method=solver, tol=tolerance, maxiter=numIter)
+        # print model output
         print(res.summary(), flush=True)
 
     def glmnetFit(self):
         X = np.array(pd.get_dummies(self.df[["gene", "codon"]]))
         y = np.array(self.df["ribosome_count"])
         lambdas = [1600, 400, 200] + list(range(100, 0, -1))
-        m = PoissonNet(n_splits=0, alpha=0,lambda_path=lambdas)
+        m = PoissonNet(n_splits=0, alpha=0, lambda_path=lambdas)
         m = m.fit(X, y)
         coefs = m.coef_path_
         print(coefs)
 
     def checkrpy(self):
+        """
+
+        :return:
+        """
         # import R's utility package
         utils = rpackages.importr('utils')
         # select a mirror for R packages
         utils.chooseCRANmirror(ind=1)  # select the first mirror in the list
         # R package names
-        packnames = ('glmnet')
+        packnames = ['glmnet']
         # R vector of strings
         from rpy2.robjects.vectors import StrVector
         # Selectively install what needs to be install.
@@ -162,47 +169,51 @@ class glmTE(object):
             utils.install_packages(StrVector(packnames), dependencies=True, repos='http://cran.rstudio.com/')
 
     def rpyFit(self):
+        """
+
+        :return:
+        """
         n2r.activate()
         r = ro.r
         r.library('glmnet')
         #
-        #from rpy2.robjects import pandas2ri
-        #pandas2ri.activate()
+        # from rpy2.robjects import pandas2ri
+        # pandas2ri.activate()
         #
         self.df = pd.read_table("filtered.txt",  header=0)
-        offsets = ro.FloatVector(list(self.df.logTPM_scaled.transpose()))
+        # offsets = ro.FloatVector(list(self.df.logTPM_scaled.transpose()))
         X = np.array(pd.get_dummies(self.df[["gene", "codon"]]))
         y = np.array(self.df["ribosome_count"])
         y = ro.IntVector(list(y.transpose()))  # use factors
         fit = r['cv.glmnet'](X, y, family="poisson", alpha=0, parallel=True)
-        #fit = r['cv.glmnet'](X, y, family="poisson", alpha=0, offset=offsets, parallel=True)
+        # fit = r['cv.glmnet'](X, y, family="poisson", alpha=0, offset=offsets, parallel=True)
         fit = np.asanyarray(fit.rx2('lambda'))
         print(fit)
-        #lambdas = [1600, 400, 200] + list(range(100, 0, -1))
+        # lambdas = [1600, 400, 200] + list(range(100, 0, -1))
 
 
-## the main process
+# main
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", help="input data-frame for modelling")
-    parser.add_argument("-u", help="unmappable regions")
+    parser.add_argument("-u", help="un-mappable regions")
     parser.add_argument("-l", help="lower bound of RNA tpm", default=1, type=int)
-    ## check if there is any argument
+    # check if there is any argument
     if len(sys.argv) <= 1:
         parser.print_usage()
         sys.exit(1)
     else:
         args = parser.parse_args()
-    ## process the file if the input files exist
-    if (args.i!=None) & (args.u!=None):
-        ## Load the content of the table
-        sys.stderr.write("[status]\tstatsmodel version:\t" + str(statsmodels.__version__) + "\n")
+    # process the file if the input files exist
+    if (args.i is not None) & (args.u is not None):
+        # Load the content of the table
+        sys.stderr.write("[status]\tstatsmodels version:\t" + str(statsmodels.__version__) + "\n")
         sys.stderr.write("[status]\tReading the ribosome counts: " + str(args.i) + "\n")
         sys.stderr.write("[status]\tReading the un-mappable regions: " + str(args.u) + "\n")
         df_fn = args.i
         unmap_fn = args.u
         tpm_lb = args.l
-        ## start model fitting
+        # start model fitting
         sys.stderr.write("[execute]\tStart the modelling of TE" + "\n")
         mod = glmTE(df_fn, unmap_fn, tpm_lb)
         sys.stderr.write("[execute]\tLoading data" + "\n")
@@ -212,8 +223,8 @@ if __name__ == '__main__':
         sys.stderr.write("[execute]\tScaling the variables" + "\n")
         mod.varScaling()
         sys.stderr.write("[execute]\tFitting the GLM" + "\n")
-        #mod.checkrpy()
-        #mod.rpyFit()
+        # mod.checkrpy()
+        # mod.rpyFit()
     else:
         sys.stderr.write("[error]\tmissing argument" + "\n")
         parser.print_usage()
