9135e787ccd9c71d61a9f605d4bfccf5b7bdd4be
hanfang <hanfang.cshl@gmail.com>
2017-03-02 17:11:45 -0500

    [update] fit model w/ glmnet cv

--- scripts/TE_modelling.py
+++ scripts/TE_modelling.py
@@ -24,6 +24,10 @@ from scipy import sparse
 import glmnet_python.glmnet as glmnet
 import glmnet_python.dataprocess as dataprocess
 import glmnet_python.glmnetCoef as glmnetCoef
+import glmnet_python.cvglmnet as cvglmnet
+import glmnet_python.cvglmnetCoef as cvglmnetCoef
+import glmnet_python.cvglmnetPlot as cvglmnetPlot
+import glmnet_python.cvglmnetPredict as cvglmnetPredict
 #import statsmodels
 #import statsmodels.api as sm
 #import statsmodels.formula.api as smf
@@ -151,29 +155,51 @@ class modelTE(object):
         return arr.tocsc()
 
     def glmnetFit(self):
-        #self.df = pd.read_table("filtered.txt",  header=0)
-        # prepare input arrays
+        # get col names
+        self.df = pd.read_table("filtered.txt",  header=0)
+        numGenes, numCodons = len(self.df.gene.unique()), len(self.df.codon.unique())
         cateVars = pd.get_dummies(self.df[["gene", "codon"]], sparse=True)
-        # numGenes, numCodons = len(self.df.gene.unique()), len(self.df.codon.unique())
         cateVarsNames = list(cateVars.columns.values)
-        # cateVarsNames = cateVarsNames[1:numGenes] + cateVarsNames[numGenes+1:numGenes+numCodons]
+        varsNames = cateVarsNames + ["secondary_structure"]
+        # prepare input arrays
         avgProb = self.df["avgProb_scaled"]
         avgProbArr = np.array(avgProb.as_matrix().reshape(len(avgProb), 1), dtype=np.float64)
         cateVars = dataprocess().sparseDf(cateVars)
         X = sparse.hstack([cateVars, avgProbArr], format="csc", dtype=np.float64)
-        varsNames = cateVarsNames + ["pairing_probability"]
         y = np.array(self.df["ribosome_count"], dtype=np.float64)
-        offsets = np.array(self.df["logTPM_scaled"], dtype=np.float64)
+        offsets = np.array(self.df["logTPM_scaled"], dtype=np.float64).reshape(len(self.df["logTPM_scaled"]), 1)
         # fit the model
-        lambdas = np.array([1600, 400, 200] + list(range(100, 0, -1)))
-        fit = glmnet(x=X.copy(), y=y.copy(), family='poisson', offset=offsets, alpha=0, lambdau=lambdas)
-        coefs = glmnetCoef(fit, s=scipy.float64([0]))
-        # print(len(varsNames), len(coefs))
-        for i in range(1, len(coefs)):
-            lst = [str(varsNames[i]), str(coefs[i][0])]
-            print("\t".join(lst))
-        #betas = [varsNames, list(coefs)]
-        #print(pd.DataFrame(betas))
+        cvfit = cvglmnet(x=X.copy(), y=y.copy(), family='poisson', offset=offsets, alpha=0, parallel=True, lambda_min=np.array([0]))
+        cvcoefs = cvglmnetCoef(cvfit, s=cvfit['lambda_min'])
+        # parse and scale coefficients
+        intercept = cvcoefs[0][0]
+        geneBetas = pd.DataFrame([[varsNames[i-1].split("_")[1], cvcoefs[i][0]] for i in range(1, numGenes)], columns=["gene", "beta"])
+        geneBetas["log2_TE"] = (geneBetas["beta"] - np.median(geneBetas["beta"])) / np.log(2)
+        geneBetas.drop(["beta"], inplace=True, axis=1)
+        codonBetas = pd.DataFrame([[varsNames[i-1].split("_")[1], cvcoefs[i][0]] for i in range(numGenes, numGenes + numCodons)], columns=["codon", "beta"])
+        codonBetas["log_codon_elongation_rate"] = (codonBetas["beta"] - np.median(codonBetas["beta"]))
+        codonBetas["codon_elongation_rate"] = np.exp(codonBetas["log_codon_elongation_rate"])
+        codonBetas.drop(["beta", "log_codon_elongation_rate"], inplace=True, axis=1)
+        pairProbBeta = cvcoefs[-1][0]
+        # export to local
+        geneBetas.to_csv(path_or_buf='genesTE.csv', sep='\t', header=True, index=False, float_format='%.4f')
+        codonBetas.to_csv(path_or_buf='codons.csv', sep='\t', header=True, index=False, float_format='%.4f')
+        # parse prefix
+        base, dir = os.path.basename(self.fn), os.path.dirname(self.fn)
+        prefix = dir + "/" + os.path.splitext(base)[0]
+        # plot
+        plt.figure()
+        cvglmnetPlot(cvfit)
+        plt.gcf()
+        plt.savefig(prefix + ".lambda_cv.pdf")
+        plt.clf()
+        # print results
+        sys.stderr.write("[results]\tlambda that gives minimum mean cv error: " + str(cvfit['lambda_min']) + "\n")
+        sys.stderr.write("[results]\tintercept: " + str(intercept) + "\n")
+        sys.stderr.write("[results]\tbeta for secondary structure pairing probability: " + str(pairProbBeta) + "\n")
+        # lambdas = np.concatenate((np.arange(1600, 100, -400), np.arange(100, 1, -1), np.arange(1, 0, -0.01), np.array([0])))
+        # fit = glmnet(x=X.copy(), y=y.copy(), family='poisson', offset=offsets, alpha=0, lambda_min=np.array([0]))# lambdau=lambdas)
+        # coefs = glmnetCoef(fit, s=scipy.float64([0]))
 
 # main
 if __name__ == '__main__':
@@ -199,11 +225,11 @@ if __name__ == '__main__':
         sys.stderr.write("[execute]\tStart the modelling of TE" + "\n")
         mod = modelTE(df_fn, unmap_fn, tpm_lb)
         sys.stderr.write("[execute]\tLoading data" + "\n")
-        mod.loadDat()
+        #mod.loadDat()
         sys.stderr.write("[execute]\tFiltering the df" + "\n")
-        mod.filterDf()
+        #mod.filterDf()
         sys.stderr.write("[execute]\tScaling the variables" + "\n")
-        mod.varScaling()
+        #mod.varScaling()
         sys.stderr.write("[execute]\tFitting the GLM" + "\n")
         mod.glmnetFit()
     else:
